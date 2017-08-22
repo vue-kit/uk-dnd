8b34e721e0e7e630691a63af76b632d42c3e5e3a
lixucheng <lixucheng@aliyun.com>
2017-08-22 18:06:22 +0800

    refine code and update overflow of dnd zone to auto

--- src/uk-dnd.vue
+++ src/uk-dnd.vue
@@ -3,21 +3,6 @@
         slot
 </template>
 <script>
-    function normalizedBoolean(boolean) {
-        return typeof boolean === "string" ? boolean === "true" : boolean;
-    }
-    function detectInTarget(node, originZone, targetZone) {
-        let originRect = originZone.getBoundingClientRect();
-        let targetRect = targetZone.getBoundingClientRect();
-        let x = node.x + (originRect.x - targetRect.x);
-        let y = node.y + (originRect.y - targetRect.y);
-        return {
-            relativeToTargetX: x,
-            relativeToTargetY: y,
-            inHorizontal: x >= 0 && (x + node.width) <= targetRect.width,
-            inVertical: y >= 0 && (y + node.height) <= targetRect.height
-        }
-    }
     export default {
         name: "uk-dnd",
         props: {
@@ -51,13 +36,6 @@
                         (/^\d+(\.\d+)?vh|%|cm|mm|in|px$/.test(val) && parseFloat(val) > 0);
                 }
             },
-            draggable: {
-                type: [String, Boolean],
-                default: true,
-                validator(val) {
-                    return typeof val === "boolean" || val === "true" || val === "false";
-                }
-            },
             clone: {
                 type: [String, Boolean],
                 default: true,
@@ -75,46 +53,46 @@
         },
         data() {
             return {
-                top: this.y,
-                left: this.x,
+                left: parseInt(this.x),
+                top: parseInt(this.y),
                 w: this.width,
                 h: this.height,
-                enableDrag: this.draggable,
-                enableClone: this.clone,
-                dndZone: this.target,
                 zIndex: 1,
                 dragging: false,
-                mouseX: 0,
-                mouseY: 0,
                 $clonedNode: null
             }
         },
         computed: {
-            normalizedDraggable() {
-                return normalizedBoolean(this.enableDrag);
+            cloneable() {
+                return typeof this.clone === "string" ? this.clone === "true" : this.clone;
             },
-            normalizedClone() {
-                return normalizedBoolean(this.enableClone);
-            },
-            normalizedDndZone() {
-                if (this.dndZone) {
-                    if (this.dndZone.substring(0, 1) === "#") {
-                        return document.getElementById(this.dndZone.substring(1));
-                    } else if (this.dndZone.substring(0, 1) === ".") {
-                        let elms = document.getElementsByClassName(this.dndZone.substring(1));
-                        if (elms.length) return elms[0];
+            dndZone() {
+                if (this.target && this.target.length > 1) {
+                    if (/^#/.test(this.target)) {
+                        let ele = document.getElementById(this.target.substring(1));
+                        if (ele.style.overflow != "auto" && ele.style.overflow != "scroll") {
+                            ele.style.overflow = "auto";
+                        }
+                        return ele;
+                    } else if (/^\./.test(this.target)) {
+                        let elms = document.getElementsByClassName(this.target.substring(1));
+                        if (elms.length) {
+                            if (elms[0].style.overflow != "auto" && elms[0].style.overflow != "scroll") {
+                                elms[0].style.overflow = "auto";
+                            }
+                            return elms[0];
+                        }
                     }
                 }
                 return null;
             },
             style() {
                 return {
-                    top: isNaN(this.top) ? this.top : this.top + "px",
-                    left: isNaN(this.left) ? this.left : this.left + "px",
+                    top: this.top + "px",
+                    left: this.left + "px",
                     width: isNaN(this.w) ? this.w : this.w + "px",
                     height: isNaN(this.h) ? this.h : this.h + "px",
-                    zIndex: this.zIndex,
-                    cursor: this.normalizedDraggable ? "move" : "default"
+                    zIndex: this.zIndex
                 }
             }
         },
@@ -133,85 +111,57 @@
         },
         methods: {
             dragstart(evt) {
-                if (this.normalizedDraggable) {
-                    this.dragging = true;
-                    this.mouseX = evt.clientX;
-                    this.mouseY = evt.clientY;
-                    if (this.normalizedClone) {
-                        let $parent = this.$el.parentNode;
-                        this.$clonedNode = this.$el.cloneNode(true);
-                        this.$clonedNode.style.opacity = .6;
-                        this.$clonedNode.style.zIndex = 1000;
-                        this.$clonedNode.style.cursor = "grabbing";
-                        $parent.appendChild(this.$clonedNode);
-                    } else {
-                        this.zIndex += 1;
-                    }
+                this.dragging = true;
+                if (this.cloneable) {
+                    document.documentElement.style.cursor = "grabbing";
+                    let $parent = this.$el.parentNode;
+                    this.$clonedNode = this.$el.cloneNode(true);
+                    this.$clonedNode.style.opacity = .6;
+                    this.$clonedNode.style.zIndex = 1000;
+                    this.$clonedNode.style.cursor = "grabbing";
+                    $parent.appendChild(this.$clonedNode);
+                } else {
+                    document.documentElement.style.cursor = "move";
+                    this.zIndex += 1;
                 }
             },
             drag(evt) {
-                if (this.normalizedDraggable && this.dragging) {
-                    let diffX = evt.clientX - this.mouseX;
-                    let diffY = evt.clientY - this.mouseY;
-                    if (this.normalizedClone) {
-                        this.$clonedNode.style.top = (parseInt(this.$clonedNode.style.top) + diffY) + "px";
-                        this.$clonedNode.style.left = (parseInt(this.$clonedNode.style.left) + diffX) + "px";
+                if (this.dragging) {
+                    if (this.cloneable) {
+                        this.$clonedNode.style.top =
+                            (parseInt(this.$clonedNode.style.top) + evt.movementY) + "px";
+                        this.$clonedNode.style.left =
+                            (parseInt(this.$clonedNode.style.left) + evt.movementX) + "px";
                     } else {
-                        let offsetX = +this.left + +diffX;
-                        let offsetY = +this.top + +diffY;
-                        if (this.normalizedDndZone) {
-                            let node = {
-                                x: offsetX,
-                                y: offsetY,
-                                width: this.w,
-                                height: this.h
-                            }
-                            let detection = detectInTarget(node, this.$el.parentNode, this.normalizedDndZone);
-                            if (detection.inHorizontal) {
-                                this.left = offsetX;
-                            }
-                            if (detection.inVertical) {
-                                this.top = offsetY;
-                            }
-                        } else {
-                            this.top = offsetY;
-                            this.left = offsetX;
-                        }
+                        let offsetX = this.left + evt.movementX;
+                        let offsetY = this.top + evt.movementY;
+                        if (offsetX >= 0) this.left = offsetX;
+                        if (offsetY >= 0) this.top = offsetY;
                     }
-                    this.mouseX = evt.clientX;
-                    this.mouseY = evt.clientY;
                 }
             },
             dragend(evt) {
-                if (this.normalizedDraggable && this.dragging) {
+                if (this.dragging) {
                     this.dragging = false;
-                    let _x, _y;
-                    if (this.normalizedClone) {
-                        _x = parseInt(this.$clonedNode.style.left);
-                        _y = parseInt(this.$clonedNode.style.top);
+                    document.documentElement.style.cursor = "auto";
+                    if (this.cloneable) {
                         this.$clonedNode.parentNode.removeChild(this.$clonedNode);
+                        if (this.dndZone) {
+                            let x = parseInt(this.$clonedNode.style.left);
+                            let y = parseInt(this.$clonedNode.style.top);
+                            let originRect = this.$el.parentNode.getBoundingClientRect();
+                            let targetRect = this.dndZone.getBoundingClientRect();
+                            let offsetX = x + (originRect.x - targetRect.x) + this.dndZone.scrollLeft;
+                            let offsetY = y + (originRect.y - targetRect.y) + this.dndZone.scrollTop;
+                            if (offsetX >=0 && offsetY >= 0) {
+                                this.$emit("drop-to-target",
+                                            offsetX, offsetY,
+                                            this.w, this.h,
+                                            this.$slots.default);
+                            }
+                        }
                     } else {
                         this.zIndex -= 1;
-                        _x = parseInt(this.left);
-                        _y = parseInt(this.top);
-                    }
-                    if (this.normalizedDndZone) {
-                        let node = {
-                            x: _x,
-                            y: _y,
-                            width: this.w,
-                            height: this.h
-                        }
-                        let detection = detectInTarget(node, this.$el.parentNode, this.normalizedDndZone);
-                        //If node drop to the target, fire the event
-                        if (detection.inHorizontal && detection.inVertical) {
-                            this.$emit("drop-to-target",
-                                        detection.relativeToTargetX,
-                                        detection.relativeToTargetY,
-                                        this.w,
-                                        this.h,
-                                        this.$slots.default);
-                        }
                     }
                 }
             }
@@ -222,5 +172,6 @@
     .uk-draggable {
         position: absolute;
         box-sizing: border-box;
+        cursor: move;
     }
 </style>
